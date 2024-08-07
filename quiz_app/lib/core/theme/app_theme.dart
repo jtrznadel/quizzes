aef569d6ae2d4b3d3361b36d5f47b2b94d5d7bcf
ozarskiw <ozarskiw@gmail.com>
2024-08-07 10:49:53 +0200

    Separate AppColorScheme class with app colors

--- quiz_app/lib/core/theme/app_theme.dart
+++ quiz_app/lib/core/theme/app_theme.dart
@@ -1,77 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'app_color_scheme.dart';
 
 final ThemeData theme = ThemeData(
   brightness: Brightness.light,
-    colorScheme: const ColorScheme.light(
-        primary: Color(0xFF116DF6),
-        onPrimary: Color(0xFFFDFEFF),
-        secondary: Color(0xFFE2F2FF),
-        onSecondary: Color(0xFF116DF6),
-        surface: Color(0xFFF4F6F7),
-        surfaceContainer: Color(0xFFFDFEFF),
-        ),
-    textTheme: TextTheme(
-      headlineLarge: TextStyle(
+  colorScheme: const ColorScheme.light(
+    primary: AppColorScheme.primary,
+    onPrimary: AppColorScheme.onPrimary,
+    secondary: AppColorScheme.secondary,
+    onSecondary: AppColorScheme.onSecondary,
+    surface: AppColorScheme.surface,
+    surfaceContainer: AppColorScheme.surfaceContainer,
+  ),
+  textTheme: TextTheme(
+    headlineLarge: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontWeight: FontWeight.w700,
         fontSize: 32,
         height: 1.2,
-        color: const Color(0xFF2E2E2E)
-      ),
-      headlineMedium: TextStyle(
+        color: AppColorScheme.textPrimary),
+    headlineMedium: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontStyle: GoogleFonts.inter().fontStyle,
         fontSize: 18,
         fontWeight: FontWeight.w700,
         height: 1.2,
-        color: const Color(0xFF2E2E2E)
-      ),
-      headlineSmall: TextStyle(
+        color: AppColorScheme.textPrimary),
+    headlineSmall: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontStyle: GoogleFonts.inter().fontStyle,
         fontSize: 14,
         fontWeight: FontWeight.w700,
         height: 1.2,
-        color: const Color(0xFF2E2E2E)
-      ),
-      bodyLarge: TextStyle(
+        color: AppColorScheme.textPrimary),
+    bodyLarge: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontSize: 18,
         fontWeight: FontWeight.w400,
         height: 1.5,
-        color: const Color(0xFF2E2E2E)
-      ),
-      bodyMedium: TextStyle(
+        color: AppColorScheme.textPrimary),
+    bodyMedium: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontSize: 16,
         fontWeight: FontWeight.w400,
         height: 1.5,
-        color: const Color(0xFF2E2E2E)
-      ),
-      bodySmall: TextStyle(
+        color: AppColorScheme.textPrimary),
+    bodySmall: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontSize: 14,
         fontWeight: FontWeight.w400,
         height: 1.5,
-        color: const Color(0xFF2E2E2E)
-      ),
-      labelLarge: TextStyle(
+        color: AppColorScheme.textPrimary),
+    labelLarge: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontSize: 18,
         fontWeight: FontWeight.w700,
-        color: const Color(0xFF2E2E2E)
-      ),
-      labelMedium: TextStyle(
+        color: AppColorScheme.textPrimary),
+    labelMedium: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontSize: 16,
         fontWeight: FontWeight.w700,
-        color: const Color(0xFF2E2E2E)
-      ),
-      labelSmall: TextStyle(
+        color: AppColorScheme.textPrimary),
+    labelSmall: TextStyle(
         fontFamily: GoogleFonts.inter().fontFamily,
         fontSize: 14,
         fontWeight: FontWeight.w700,
-        color: const Color(0xFF2E2E2E)
-      ),
-    ));
+        color: AppColorScheme.textPrimary),
+  ),
+);
