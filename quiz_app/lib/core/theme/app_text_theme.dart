24bf76d125bd7ff434857bd1fb851808c002d74c
jtrznadel <trznadeljakub@outlook.com>
2024-08-08 10:24:45 +0200

    fix text theme

--- quiz_app/lib/core/theme/app_text_theme.dart
+++ quiz_app/lib/core/theme/app_text_theme.dart
@@ -3,48 +3,13 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/core/theme/app_color_scheme.dart';
 
 final TextTheme textTheme = TextTheme(
-  headlineLarge: TextStyle(
-      fontFamily: GoogleFonts.inter().fontFamily,
-      fontWeight: FontWeight.w700,
-      fontSize: 32,
-      height: 1.2,
-      color: AppColorScheme.textPrimary),
-  headlineMedium: TextStyle(
-      fontFamily: GoogleFonts.inter().fontFamily,
-      fontStyle: GoogleFonts.inter().fontStyle,
-      fontSize: 18,
-      fontWeight: FontWeight.w700,
-      height: 1.2,
-      color: AppColorScheme.textPrimary),
-  headlineSmall: TextStyle(
-      fontFamily: GoogleFonts.inter().fontFamily,
-      fontStyle: GoogleFonts.inter().fontStyle,
-      fontSize: 14,
-      fontWeight: FontWeight.w700,
-      height: 1.2,
-      color: AppColorScheme.textPrimary),
-  bodyLarge: TextStyle(
-      fontFamily: GoogleFonts.inter().fontFamily,
-      fontSize: 18,
-      fontWeight: FontWeight.w400,
-      height: 1.5,
-      color: AppColorScheme.textPrimary),
-  bodyMedium: TextStyle(
-      fontFamily: GoogleFonts.inter().fontFamily,
-      fontSize: 16,
-      fontWeight: FontWeight.w400,
-      height: 1.5,
-      color: AppColorScheme.textPrimary),
-  bodySmall: TextStyle(
-      fontFamily: GoogleFonts.inter().fontFamily,
-      fontSize: 14,
-      fontWeight: FontWeight.w400,
-      height: 1.5,
-      color: AppColorScheme.textPrimary),
-  labelLarge:
-      TextStyle(fontFamily: GoogleFonts.inter().fontFamily, fontSize: 18, fontWeight: FontWeight.w700, color: AppColorScheme.textPrimary),
-  labelMedium:
-      TextStyle(fontFamily: GoogleFonts.inter().fontFamily, fontSize: 16, fontWeight: FontWeight.w700, color: AppColorScheme.textPrimary),
-  labelSmall:
-      TextStyle(fontFamily: GoogleFonts.inter().fontFamily, fontSize: 14, fontWeight: FontWeight.w700, color: AppColorScheme.textPrimary),
+  headlineLarge: GoogleFonts.inter(fontWeight: FontWeight.w700, fontSize: 32, height: 1.2, color: AppColorScheme.textPrimary),
+  headlineMedium: GoogleFonts.inter(fontSize: 18, fontWeight: FontWeight.w700, height: 1.2, color: AppColorScheme.textPrimary),
+  headlineSmall: GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.w700, height: 1.2, color: AppColorScheme.textPrimary),
+  bodyLarge: GoogleFonts.inter(fontSize: 18, fontWeight: FontWeight.w400, height: 1.5, color: AppColorScheme.textPrimary),
+  bodyMedium: GoogleFonts.inter(fontSize: 16, fontWeight: FontWeight.w400, height: 1.5, color: AppColorScheme.textPrimary),
+  bodySmall: GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.w400, height: 1.5, color: AppColorScheme.textPrimary),
+  labelLarge: GoogleFonts.inter(fontSize: 18, fontWeight: FontWeight.w700, color: AppColorScheme.textPrimary),
+  labelMedium: GoogleFonts.inter(fontSize: 16, fontWeight: FontWeight.w700, color: AppColorScheme.textPrimary),
+  labelSmall: GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.w700, color: AppColorScheme.textPrimary),
 );
