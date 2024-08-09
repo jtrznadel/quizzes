0aa8eaa5e5a7ef533a291d68735f24720de7d0e1
jtrznadel <trznadeljakub@outlook.com>
2024-08-09 11:57:51 +0200

    implement create-quizz base config

---
+++ quiz_app/lib/features/quizz/presentation/views/quizz_creation_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:quiz_app/core/common/widgets/basic_app_bar.dart';
+import 'package:quiz_app/core/extensions/context_extension.dart';
+import 'package:quiz_app/core/theme/app_color_scheme.dart';
+import 'package:quiz_app/features/quizz/presentation/views/quizz_configure_screen.dart';
+import 'package:quiz_app/features/quizz/presentation/views/quizz_preview_screen.dart';
+import 'package:quiz_app/features/quizz/presentation/views/quizz_text_prompt_screen.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class QuizzCreationScreen extends StatefulWidget {
+  const QuizzCreationScreen({super.key});
+
+  static const routeName = '/quizz-creation';
+
+  @override
+  State<QuizzCreationScreen> createState() => _QuizzCreationScreenState();
+}
+
+class _QuizzCreationScreenState extends State<QuizzCreationScreen> {
+  final _controller = PageController();
+  final int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  final pages = [
+    const QuizzTextPromptScreen(),
+    const QuizzConfigureScreen(),
+    const QuizzPreviewScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const BasicAppBar(title: 'Create quizz'),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SmoothPageIndicator(
+              controller: _controller,
+              count: pages.length,
+              effect: const WormEffect(
+                dotColor: AppColorScheme.secondary,
+                activeDotColor: AppColorScheme.primary,
+                dotHeight: 8,
+                dotWidth: 32,
+              ),
+            ),
+          ),
+          PageView(
+            controller: _controller,
+            children: pages,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- quiz_app/lib/temp.dart
+++ quiz_app/lib/temp.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/core/extensions/context_extension.dart';
 import 'package:quiz_app/features/auth/presentation/views/forgot_pasword_screen.dart';
 import 'package:quiz_app/features/auth/presentation/views/new_password_screen.dart';
 import 'package:quiz_app/features/auth/presentation/views/sign_in_screen.dart';
 import 'package:quiz_app/features/auth/presentation/views/successful_password_reset_request_screen.dart';
+import 'package:quiz_app/features/quizz/presentation/views/quizz_creation_screen.dart';
 
 class TempScreen extends StatelessWidget {
   const TempScreen({super.key});
@@ -21,30 +23,34 @@ class TempScreen extends StatelessWidget {
                   Navigator.of(context).pushNamed('/welcome');
                 },
                 child: const Text('Welcome page')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(SignInScreen.routeName);
-                },
-                child: const Text('Login page')),
+            // ElevatedButton(
+            //     onPressed: () {
+            //       Navigator.of(context).pushNamed(SignInScreen.routeName);
+            //     },
+            //     child: const Text('Login page')),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context).pushNamed(ForgotPasswordScreen.routeName);
+            //   },
+            //   child: const Text('Forgot password page'),
+            // ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context).pushNamed(SuccessfulPasswordResetRequestScreen.routeName);
+            //   },
+            //   child: const Text('Reset password page'),
+            // ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context).pushNamed(NewPasswordScreen.routeName);
+            //   },
+            //   child: const Text('New password screen'),
+            // ),
             ElevatedButton(
               onPressed: () {
-                Navigator.of(context)
-                    .pushNamed(ForgotPasswordScreen.routeName);
+                context.navigator.pushNamed(QuizzCreationScreen.routeName);
               },
-              child: const Text('Forgot password page'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .pushNamed(SuccessfulPasswordResetRequestScreen.routeName);
-              },
-              child: const Text('Reset password page'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(NewPasswordScreen.routeName);
-              },
-              child: const Text('New password screen'),
+              child: const Text('Quiz'),
             ),
           ],
         ),
