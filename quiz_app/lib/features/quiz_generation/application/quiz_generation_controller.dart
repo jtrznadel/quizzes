0ea4541cb30d2b26c1b6a413da79b29a6b7bacf4
ozarskiw <ozarskiw@gmail.com>
2024-08-30 09:59:55 +0200

    Implemented quiz generation state and controller classes

---
+++ quiz_app/lib/features/quiz_generation/application/quiz_generation_controller.dart
@@ -0,0 +1,149 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../generated/l10n.dart';
+import '../data/repositories/quiz_generation_repository.dart';
+import '../domain/answer_model.dart';
+import '../domain/question_model.dart';
+import '../domain/quiz_generation_model.dart';
+import '../domain/quiz_model.dart';
+import 'quiz_generation_state.dart';
+
+part 'quiz_generation_controller.g.dart';
+
+@riverpod
+class QuizGenerationController extends _$QuizGenerationController {
+  late QuizGenerationModel _quizModel;
+  late QuizModel _quiz;
+
+  @override
+  QuizGenerationState build() {
+    _quizModel = const QuizGenerationModel(
+      content: '',
+      typeOfQuestions: 0,
+      numberOfQuestions: 3,
+    );
+    _quiz = const QuizModel(title: '', description: '', generateQuestionsDto: []);
+    return const QuizGenerationState.generating();
+  }
+
+  String get content => _quizModel.content;
+  int get typeOfQuestions => _quizModel.typeOfQuestions;
+  int get numberOfQuestions => _quizModel.numberOfQuestions;
+
+  String get title => _quiz.title;
+  String get description => _quiz.description;
+  List<QuestionModel> get questions => _quiz.generateQuestionsDto;
+
+  void setContent(String content) {
+    _quizModel = _quizModel.copyWith(content: content);
+  }
+
+  void setTypeOfQuestions(int typeOfQuestions) {
+    _quizModel = _quizModel.copyWith(typeOfQuestions: typeOfQuestions);
+  }
+
+  void setNumberOfQuestions(int numberOfQuestions) {
+    _quizModel = _quizModel.copyWith(numberOfQuestions: numberOfQuestions);
+  }
+
+  void addNewQuestion(QuestionModel question){
+    _quiz = _quiz.copyWith(generateQuestionsDto: [..._quiz.generateQuestionsDto, question]);
+    state = QuizGenerationState.generated(_quiz);
+  }
+
+  //TODO: remove after testing
+  Map<String, dynamic> quizToJson(){
+    return {
+      'title': _quiz.title,
+      'description': _quiz.description,
+      'createQuestionsDto': [
+        for (var question in _quiz.generateQuestionsDto)
+          {
+            'title': question.title,
+            'createAnswersDto': [
+              for (var answer in question.generateAnswersDto)
+                {
+                  'content': answer.content,
+                  'isCorrect': answer.isCorrect,
+                }
+            ],
+          }
+      ],
+    };
+  }
+
+
+  Future<void> generate() async {
+    state = const QuizGenerationState.generating();
+
+    //TODO: remove after testing
+    _quiz = mockQuiz;
+    state = QuizGenerationState.generated(mockQuiz);
+    return;
+
+    final result = await ref
+        .read(quizGenerationRepositoryProvider)
+        .generateQuiz(quizGenerationModel: _quizModel);
+
+    result.fold(
+      (error) {
+        //TODO: replace with translation
+        state = const QuizGenerationState.error('Something went wrong');
+      },
+      (quiz) {
+        state = QuizGenerationState.generated(quiz);
+        _quiz = quiz;
+      },
+    );
+  }
+}
+
+
+QuizModel mockQuiz = QuizModel(
+  title: S.current.tempQuizzSummaryTitle,
+  description: S.current.tempQuizzSummaryDescription,
+  generateQuestionsDto: [
+    QuestionModel(
+      title: S.current.tempQuestion,
+      generateAnswersDto: [
+        AnswerModel(
+          content: S.current.tempAnswer1,
+          isCorrect: false,
+        ),
+        AnswerModel(
+          content: S.current.tempAnswer2,
+          isCorrect: true,
+        ),
+        AnswerModel(
+          content: S.current.tempAnswer3,
+          isCorrect: false,
+        ),
+        AnswerModel(
+          content: S.current.tempAnswer4,
+          isCorrect: false,
+        ),
+      ],
+    ),
+    QuestionModel(
+      title: S.current.tempQuestion,
+      generateAnswersDto: [
+        AnswerModel(
+          content: S.current.tempAnswer1,
+          isCorrect: false,
+        ),
+        AnswerModel(
+          content: S.current.tempAnswer2,
+          isCorrect: true,
+        ),
+        AnswerModel(
+          content: S.current.tempAnswer3,
+          isCorrect: false,
+        ),
+        AnswerModel(
+          content: S.current.tempAnswer4,
+          isCorrect: false,
+        ),
+      ],
+    ),
+  ],
+);
---
+++ quiz_app/lib/features/quiz_generation/application/quiz_generation_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'quiz_generation_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$quizGenerationControllerHash() =>
+    r'fce79a54733c75fddaae619b0a758019c45e5bfa';
+
+/// See also [QuizGenerationController].
+@ProviderFor(QuizGenerationController)
+final quizGenerationControllerProvider = AutoDisposeNotifierProvider<
+    QuizGenerationController, QuizGenerationState>.internal(
+  QuizGenerationController.new,
+  name: r'quizGenerationControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$quizGenerationControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$QuizGenerationController = AutoDisposeNotifier<QuizGenerationState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
